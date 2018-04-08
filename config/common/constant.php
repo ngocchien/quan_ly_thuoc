cb54f39c00317b91e3c2a0ad411e6eab3ff2273f
chiennn <ngocchien01@gmail.com>
2018-04-08 20:34:18 +0700

    fix layout

--- config/common/constant.php
+++ config/common/constant.php
@@ -1,7 +1,7 @@
 <?php
 define('KEY_PREFIX', 'thuoc_');
 define('UTF8_ENABLED', FALSE);
-define('STATIC_VERSION', '1479136141');
+define('STATIC_VERSION', '1479136150');
 
 define('STATIC_PATH', ROOT_PATH.'/static');
 define('UPLOAD_PATH', STATIC_PATH.'/uploads');
